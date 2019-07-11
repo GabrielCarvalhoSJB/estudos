c61c8a9c38551f1cea889727e7f729225173baf5
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-11 07:13:58 -0300

    Exemplo If Livro PHP Mysql

---
+++ ifPagina44.php
@@ -0,0 +1,14 @@
+<?php
+
+$i = 5 ;
+if ($i == 3){
+    echo("O valor de i é 3.");
+}else if ($i == 4){
+    echo("O valor de i é 4.");
+}else{
+    echo ("O valor de i não é 3 nem 4.");
+}
+//exemplo livro PhP e Mysql
+//pag 44 e 45.
+
+?>
