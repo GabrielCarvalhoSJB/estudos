d0eeaade435e9eec7e0fb9dfc4b7b59f24fbf933
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-13 11:06:39 -0300

    ExerciciosLivroAlgoritmos For

--- Exercicio3.php
+++ Exercicio3.php
@@ -1,6 +1,6 @@
 <?php
-//Escreva um programa que controle senha que de 3 chances de acerto
-//Se ele nao conseguir deve avisar que o cartao foi bloqueado
+//Escreva um programa que controle senha que de 3 chances de acerto,
+//Se ele nao conseguir deve avisar que o cartao foi bloqueado.
 $tentativa = 3;
 $cartao = 123;
 if($cartao == 123){
