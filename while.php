e73079190360e6a9638cb5d2e3eb083c85c1e6f4
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-18 07:08:47 -0300

    Exemplo While

--- while.php
+++ while.php
@@ -1,4 +1,8 @@
 <?php
+//While – Esta instrução é usada quando não sabemos quantas vezes um determinado bloco
+// de instruções precisa ser repetido. Com ele, a execução das instruções vai continuar até que uma condição seja verdadeira. 
+//A condição a ser analisada para a execução do laço de repetição deverá retornar um valor booleano.
+//fonte: https://www.devmedia.com.br/while-e-do-while-lacos-de-repeticoes-estrutura-da-linguagem-parte-1/18870
 $condicao = true;
 
 while ($condicao){
