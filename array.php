29e732fd1e54d407ce419d2bb1dfa67cf6b8e729
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-18 19:01:09 -0300

    Exemplos Array

---
+++ array.php
@@ -0,0 +1,21 @@
+<?php
+//Primeira regra todo array inicia com 0;
+//Segunda regra quando um array tem apenas uma dimensão é chamado de vetor
+$frutas = array("laranja", "abacaxi", "melancia");
+print_r($frutas);
+
+echo'<br>';
+
+$pessoas = array();
+
+array_push($pessoas, array(
+    'nome' => 'Gabriel',
+    'idade' => 27
+));
+
+array_push($pessoas, array(
+    'nome' => 'Gabriel Jr',
+    'idade' => 4
+));
+print_r($pessoas);
+?>
