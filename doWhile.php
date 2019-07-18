e71ca7ff0693811534a754ced20ecbd944d023cb
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-18 07:13:56 -0300

    Exemplos DoWhile

--- doWhile.php
+++ doWhile.php
@@ -1,5 +1,8 @@
 <?php
-
+//Do/While – O do/while tem quase o mesmo
+// funcionamento que o while, a diferença é que com o uso dele teremos os 
+//comandos executados ao menos uma única vez.
+//fonte: https://www.devmedia.com.br/while-e-do-while-lacos-de-repeticoes-estrutura-da-linguagem-parte-1/18870
 $total = 50;
 $desconto = 0.9;
 
