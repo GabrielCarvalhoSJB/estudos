363416d1ca78c23d53152e9514c2d7cc71a39cd7
GabrielCarvalhoSJB <gcarvalho915@gmail.com>
2019-07-26 10:08:45 -0300

    Exemplos For e Foreach

---
+++ ForForeach/for.php
@@ -0,0 +1,12 @@
+<?php
+//Comando de repetição for que em portugues PARA
+//No sentido de PARA cada um dos valores.
+
+/* for (valores_iniciais; condicao; incremento){
+    codigo a ser executado;
+} */
+
+
+for ($i = 0; $i < 20; $i++){
+    echo $i . ' '. "Gabriel" . '<br>';
+}
